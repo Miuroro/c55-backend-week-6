7486f4c4125943f46bdf6a37febd4c8d8ae5fc41
Stasel <2033301+stasel@users.noreply.github.com>
2026-06-08 17:45:23 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# Backend week 6 assignment
+Full instructions for the assignment can be found in the learning platform: https://hub.hackyourfuture.nl/backend-week-6-assignment
+
+## Implementation Instructions
+### Tasks 1 & 2
+Implement your Spring Boot application in the `Postify` directory. We have already initialized a new Spring Boot project for you.
---
+++ postify/src/main/java/net/hackyourfuture/backend/week6/postify/PostifyApplication.java
@@ -0,0 +1,13 @@
+package net.hackyourfuture.backend.week6.postify;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PostifyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PostifyApplication.class, args);
+	}
+
+}
